35596c398af6efce5bd479c09703edc3cadd122f
Omar Bajja <obajja@f4r2s7.clusters.42paris.fr>
2026-02-04 17:26:22 +0100

    vector sort done, timestamps done. TODO: Lists sort

--- C09/ex02/PmergeMe.cpp
+++ C09/ex02/PmergeMe.cpp
@@ -6,11 +6,14 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/15 12:09:46 by obajja            #+#    #+#             */
-/*   Updated: 2026/01/16 14:53:18 by obajja           ###   ########.fr       */
+/*   Updated: 2026/02/04 17:25:47 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <bits/stdc++.h>
+#include <sys/time.h>
+#include <iterator>
 
 PmergeMe::PmergeMe(){}
 
@@ -28,8 +31,34 @@ PmergeMe & PmergeMe::operator=(PmergeMe const & src)
 
 PmergeMe::~PmergeMe(){}
 
+void vector_printer(std::vector<int> & to_print, std::string message)
+{
+    std::cout << message;
+    for (size_t i = 0; i < to_print.size(); i++)
+       std::cout << to_print[i] << " ";
+    std::cout << std::endl;
+}
+
+void list_printer(std::list<int> & to_print, std::string message)
+{
+    std::cout << message;
+    std::list<int>::iterator it = to_print.begin();
+
+    for (size_t i = 0; i < to_print.size(); i++)
+       std::cout << *it++ << " ";
+    std::cout << std::endl;
+}
+
 void PmergeMe::input_sanitizer(int argc, char** av)
 {
+    struct timeval start;
+    struct timeval end;
+    struct timeval start_list;
+    struct timeval end_list;
+
+    gettimeofday(&start, NULL);
+    gettimeofday(&start_list, NULL);
+
     for (int i = 0; i < argc - 1; i++)
     {
         char *endptr;
@@ -39,20 +68,40 @@ void PmergeMe::input_sanitizer(int argc, char** av)
         else
         {
             this->_unsorted.push_back(num);
+            this->_unseasoned.push_back(num);
         }
     }
 
     _size = argc - 1;
-    
-    vector_sorter(this->_unsorted);
+    std::vector<int> sorted = vector_sorter(this->_unsorted);
+    gettimeofday(&end, NULL);
+    std::list<int> list_sorted = list_sorter(this->_unseasoned);
+    gettimeofday(&end_list, NULL);
+
+    double elapsed_time_vec = (end.tv_sec - start.tv_sec) * 1e6;
+    elapsed_time_vec = (elapsed_time_vec + (end.tv_usec - start.tv_usec));
+    double elapsed_time_lst = (end.tv_sec - start.tv_sec) * 1e6;
+    elapsed_time_lst = (elapsed_time_lst + (end.tv_usec - start.tv_usec));
+    vector_printer(_unsorted, "Before: ");
+    vector_printer(sorted, "After: ");
+    list_printer(_unseasoned, "Before: ");
+    list_printer(list_sorted, "After: ");
+    std::cout << "Time to process a range of " << _unsorted.size() << " elements with std::vector : " << std::fixed << std::setprecision(2) << elapsed_time_vec << " microseconds" << std::endl;
+    std::cout << "Time to process a range of " << _unsorted.size() << " elements with std::list : " << std::fixed << std::setprecision(2) << elapsed_time_lst << " microseconds" << std::endl;
+
 }
 
-void PmergeMe::vector_sorter(std::vector<int> & to_sort)
+std::vector<int> PmergeMe::vector_sorter(std::vector<int> & to_sort)
 {
-    //time management here
-
+    int lone = -1;
     if (to_sort.size() <= 1)
-        return;
+        return to_sort;
+
+    if (to_sort.size() % 2 != 0)
+    {
+        lone = to_sort[to_sort.size() - 1];
+        to_sort.pop_back();
+    }
 
     std::vector<int> major;
     std::vector<int> minor;
@@ -75,11 +124,13 @@ void PmergeMe::vector_sorter(std::vector<int> & to_sort)
         }
     }
 
-    vector_sorter(major);
-    
+    std::vector<int> result;
+    result = vector_sorter(major);
+    if (lone != -1)
+        minor.push_back(lone);
     std::vector<int>jacobsNum = Jacobsthal_fill(minor.size());
-    min_insertion(major, minor, jacobsNum);
-
+    min_insertion(result, minor, jacobsNum);
+    return result;
 }
 
 void    min_insertion(std::vector<int> &major, std::vector<int> &minor, std::vector<int> &jacobsNum)
@@ -101,7 +152,7 @@ void    min_insertion(std::vector<int> &major, std::vector<int> &minor, std::vec
 
     if (!minor.empty())
     {
-        for (size_t i = minor.size() - 1; i > 0; i--)
+        for (int i = minor.size() - 1; i >= 0; i--)
         {
             int to_insert = minor[i];
             int position = binary_search(major, to_insert);
@@ -133,7 +184,6 @@ void vector_insert(std::vector<int>& major, int to_insert, int position)
     major.insert(major.begin() + position, to_insert);
 }
 
-
 std::vector<int> Jacobsthal_fill(int len)
 {
     std::vector<int>result;
@@ -150,4 +200,135 @@ std::vector<int> Jacobsthal_fill(int len)
     }
 
     return (result);
+}
+
+std::list<int> PmergeMe::list_sorter(std::list<int> & to_sort)
+{
+    std::list<int>::iterator it = to_sort.begin();
+    std::list<int>::iterator it2 = it++;
+
+    int lone = -1;
+    if (to_sort.size() <= 1)
+        return to_sort;
+
+    if (to_sort.size() % 2 != 0)
+    {
+        lone = to_sort.back();
+        to_sort.pop_back();
+    }
+
+    std::list<int> major;
+    std::list<int> minor;
+
+    for (size_t i = 0; i < to_sort.size(); i += 2)
+    {
+        if (i == to_sort.size() - 1 && to_sort.size() % 2 != 0)
+        {
+            minor.splice(minor.end(), to_sort, to_sort.end());
+        }
+        else if ( *it <= *it2 )
+        {
+            minor.splice(minor.end(),to_sort, it);
+            major.splice(minor.end(),to_sort, it2);
+        }
+        else if (*it > *it2)
+        {
+            minor.splice(minor.end(),to_sort, it2);
+            major.splice(minor.end(),to_sort, it);
+        }
+        it++;
+        it2++;
+    }
+
+    std::list<int> result;
+    result = list_sorter(major);
+    if (lone != -1)
+        minor.push_back(lone);
+    std::list<int>jacobsNum = list_Jacobsthal_fill(minor.size());
+    list_min_insertion(result, minor, jacobsNum);
+    return result;
+}
+
+std::list<int> list_Jacobsthal_fill(int len)
+{
+    std::list<int>result;
+    int num = 0;
+    result.push_back(0);
+    result.push_back(1);
+    
+    int first = 0;
+    int second = 1;
+
+    for (int i = 2; i < len; i++)
+    {
+        num = second + 2 * first;
+        if (num > len)
+            break;
+        result.push_back(second + 2 * first);
+        first = second;
+        second = num;
+    }
+
+    return (result);
+}
+
+int list_binary_search(std::list<int>& major, int to_insert)
+{
+    int start = 0;
+    int end = major.size();
+    std::list<int>::iterator it = major.begin();
+    
+    while (start < end)
+    {
+        int mid = (start + end) / 2;
+        std::advance(it, mid);
+        if (to_insert < *it)
+            end = mid;
+        else
+            start = mid + 1;
+    }
+
+    return (start);
+}
+void list_insert(std::list<int>& major, int to_insert, int position)
+{
+    std::list<int>::iterator it = major.begin();
+    std::advance(it, position);
+    major.insert(it, to_insert);
+}
+void    list_min_insertion(std::list<int> &major, std::list<int> &minor, std::list<int> &jacobsNum)
+{
+    std::list<int>::iterator it = minor.begin();
+    std::list<int>::iterator loop = jacobsNum.begin();
+    std::advance(loop, 1);
+
+    for (size_t i = 2; i < jacobsNum.size(); i++)
+    {
+        if (static_cast<size_t>(*loop) <= minor.size() - 1)
+        {
+            for (int j = *loop; j > *loop; j--)
+            {
+                it = minor.begin();
+                std::advance(loop, 1);
+                std::advance(it, j);
+                int to_insert = *it;
+                int position = list_binary_search(major, to_insert);
+                list_insert(major, to_insert, position);
+                it = minor.erase(it);
+            }
+        }
+    }
+
+    if (!minor.empty())
+    {
+        for (int i = minor.size() - 1; i >= 0; i--)
+        {
+            it = minor.begin();
+            std::advance(it, i);
+            int to_insert = *it;
+            int position = list_binary_search(major, to_insert);
+            list_insert(major, to_insert, position);
+            it = minor.erase(it);
+        }
+    }
 }
--- C09/ex02/PmergeMe.hpp
+++ C09/ex02/PmergeMe.hpp
@@ -6,12 +6,13 @@
 /*   By: obajja <obajja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/14 18:12:27 by obajja            #+#    #+#             */
-/*   Updated: 2026/01/16 10:50:54 by obajja           ###   ########.fr       */
+/*   Updated: 2026/02/04 17:13:45 by obajja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <vector>
+#include <list>
 #include <cstdlib>
 #include <iostream>
 #include <limits.h>
@@ -37,15 +38,23 @@ class PmergeMe{
         };
         
         void input_sanitizer(int argc, char** av);
-        void vector_sorter(std::vector<int> & to_sort);
+        std::vector<int> vector_sorter(std::vector<int> & to_sort);
+        std::list<int> list_sorter(std::list<int> & to_sort);
+
 
     private:
         std::vector<int>    _unsorted;
+        std::list<int>      _unseasoned;
         std::vector<int>    _sorting;
         int                 _size;
 };
 
 std::vector<int> Jacobsthal_fill(int len);
-int binary_search(std::vector<int>& major, int to_insert);
-void vector_insert(std::vector<int>& major, int to_insert, int position);
+int     binary_search(std::vector<int>& major, int to_insert);
+void    vector_insert(std::vector<int>& major, int to_insert, int position);
 void    min_insertion(std::vector<int> &major, std::vector<int> &minor, std::vector<int> &jacobsNum);
+
+std::list<int> list_Jacobsthal_fill(int len);
+int     list_binary_search(std::list<int>& major, int to_insert);
+void    list_insert(std::list<int>& major, int to_insert, int position);
+void    list_min_insertion(std::list<int> &major, std::list<int> &minor, std::list<int> &jacobsNum);
